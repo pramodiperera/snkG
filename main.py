83e9020a4f2c236e7a28f107670c58877ba1b0fa
pramodiperera <hdprangana@gmail.com>
2022-01-07 15:44:27 +0530

    added the block on surface

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 import pygame
-import time
+from pygame.locals import *
+
+
+def draw_block():
+    surface.fill((255, 255, 255))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
 
 if __name__ == "__main__":
     pygame.init()
 
+    # draw the surface and colour
     surface = pygame.display.set_mode((500, 500))
     surface.fill((255, 255, 255))
+
+    # draw the block
+    block = pygame.image.load("resources/block.jpg").convert()
+    block_x = 0
+    block_y = 0
+    surface.blit(block, (block_x, block_y))
+
+    # update the surface
     pygame.display.flip()
-    time.sleep(5)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+
+            elif event.type == QUIT:
+                running = False
